ef1ec44a0dc799da920a1d94f819090364652808
mrbeaver2K <derekdelatorre@icloud.com>
2023-12-06 15:08:25 -0800

    Added functions.

--- baseconverter.py
+++ baseconverter.py
@@ -1,15 +1,27 @@
 import PySimpleGUI as sg
+
+def to_quantity(value, radix):
+    print(f"to_quantity({value=}, {radix=})")
+    return 42
+
+def to_value(quantity, radix):
+    print(f"to_value({quantity=}, {radix=}")
+    return 42
+
 sg.theme("Gray Gray Gray")
-layout = [[sg.Text("Base Converter Program (DON\'T INPUT FLOATS)         Base Input V")],
+layout = [[sg.Text("Base Converter Program (DON'T INPUT FLOATS)"), sg.Push(), sg.Text("Base Input V    ")],
         [sg.Text("Input:"), sg.Input(key="input", pad=(11, 0)), sg.Input(key="frombasein", size=2)],
         [sg.Text("Output:"), sg.Input(key="output", disabled=True, pad=0), sg.Input(key="tobasein", size=2, pad=(16, 0))],
         [sg.Button("60"), sg.Button("Close")],
-        [sg.Text("Loading Bays for Symbols:")],
-        [sg.Multiline(key="symbolsin", size=(24,6), default_text="[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]")]]
+        [sg.Text("Loading Bay for Symbols:")],
+        [sg.Multiline(key="symbolsin", size=(32, 6), default_text=str(list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")))]]
 window = sg.Window("Base Converter", layout)
+
 while True:
     event, values = window.read()
-    if event == "Close":
+    if event in ("Close", sg.WIN_CLOSED):
         break
+    if event == "60":
+        value = to_value(to_quantity(values["input"], values["frombasein"]), values["tobasein"])
+        window["output"].update(value)
 window.close()
-print("You may now exit the window.")
