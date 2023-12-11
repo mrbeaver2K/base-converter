bc9bf19d877ee23f6cd56ca0a15715040a345745
mrbeaver2K <derekdelatorre@icloud.com>
2023-12-11 15:02:05 -0800

    Fixed "Dump" and added "Swap"

--- baseconverter.py
+++ baseconverter.py
@@ -31,7 +31,7 @@ sg.theme("Gray Gray Gray")
 layout = [[sg.Text("Base Converter Program (DON'T INPUT FLOATS)"), sg.Push(), sg.Text("Base Input V    ")],
         [sg.Text("Input:"), sg.Input(key="input", pad=(11, 0)), sg.Input(key="frombasein", size=2)],
         [sg.Text("Output:"), sg.Input(key="output", disabled=True, pad=0), sg.Input(key="tobasein", size=2, pad=(16, 0))],
-        [sg.Button("60"), sg.Button("Close"), sg.Button("Dump")],
+        [sg.Button("60"), sg.Button("Close"), sg.Button("Dump"), sg.Button("Swap")],
         [sg.Text("Loading Bay for Symbols:")],
         [sg.Multiline(key="symbolsin", size=(32, 6), default_text="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")]]
 window = sg.Window("Base Converter", layout)
@@ -44,6 +44,13 @@ while True:
         value = to_value(to_quantity(values["input"], values["frombasein"]), values["tobasein"])
         window["output"].update(value)
     if event == "Dump":
-        exec("symbols = list(" + values["symbolsin"] + ")")
+        symbols = values["symbolsin"]
         print(symbols)
+    if event == "Swap":
+        window["input"].update(value)
+        temp = values["frombasein"]
+        window["frombasein"].update(values["tobasein"])
+        window["tobasein"].update(temp)
+        window["output"].update("Press 60 to recalulate")
+        del temp
 window.close()
